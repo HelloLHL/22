80dde1a659ee66a3875e6f4686dbaf60751c1f0b
hello_LHL <654787030@qq.com>
2022-10-10 17:11:08 +0800

    add

--- main.cpp
+++ main.cpp
@@ -2,25 +2,13 @@
 // Created by 李洪良 on 2022/10/10.
 //
 
-#include <version>
+#include <version.h>
 #include <iostream>
 using namespace std;
 
-#define COMPILER_FEATURE_VALUE(value) #value
-#define COMPILER_FEATURE_ENTRY(name) { #name, COMPILER_FEATURE_VALUE(name) }
-
-struct CompilerFeature {
-    CompilerFeature(const char* name = nullptr, const char* value = nullptr)
-            : name(name), value(value) {}
-
-    const char* name;
-    const char* value;
-};
 
 int main()
 {
-    CompilerFeature x = COMPILER_FEATURE_ENTRY(__cplusplus);
-    cout << "name:" << x.name << endl;
-    cout << "value:" << x.value << endl;
+    getCPPVersion();
     return 0;
 }
